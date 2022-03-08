228f521c84e6dfac3efb80a2f2fc0445f9e549be
mianmuhammad.anwar <mianmuhammad.anwar@systemsltd.com>
2022-03-08 23:29:13 +0500

    Update Credit Card detail incorporated

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -15,7 +15,7 @@ const RESOURCES = {
 "assets/assets/images/error.png": "34a3c86257800bf5e90595c112370b19",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "7520ffb0555c93d3a4a037e1bf2b3f3a",
+"assets/NOTICES": "7e74284ed0f26e21326f819322be4147",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/fluttertoast/assets/toastify.css": "a85675050054f179444bc5ad70ffc635",
 "assets/packages/fluttertoast/assets/toastify.js": "e7006a0a033d834ef9414d48db3be6fc",
@@ -24,9 +24,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "3f24760f3ad637725fbe8ff3e7ab338e",
-"/": "3f24760f3ad637725fbe8ff3e7ab338e",
-"main.dart.js": "6584d7c4fe934cf0255b733c0e25c174",
+"index.html": "a05c5cc61b9fe4304948dd6fb8319e47",
+"/": "a05c5cc61b9fe4304948dd6fb8319e47",
+"main.dart.js": "dba86cf6acf5340e43daeb40859bed99",
 "manifest.json": "2c78dcac85a0194bdaf6da48ce6cfce9",
 "version.json": "51730d72db2081539e5b4f4acd7b3dc7"
 };
